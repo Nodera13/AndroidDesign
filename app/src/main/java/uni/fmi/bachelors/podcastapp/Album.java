f0ad330df68d55dfa6b26d26200580d136de61db
Nodera13 <cveteliniliev9912@gmail.com>
2021-06-16 20:11:09 +0300

    first commit

---
+++ app/src/main/java/uni/fmi/bachelors/podcastapp/Album.java
@@ -0,0 +1,14 @@
+package uni.fmi.bachelors.podcastapp;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Album extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_album);
+    }
+}
